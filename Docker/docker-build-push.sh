9cd8e83a7a8761065e7bcf48ea5a063913cae964
Gary A. Stafford <garystafford@rochester.rr.com>
2021-05-10 22:17:25 -0400

    Updating for 2021.05 release

--- Docker/docker-build-push.sh
+++ Docker/docker-build-push.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ./gradlew clean build
-docker build -f Docker/Dockerfile --no-cache -t garystafford/storefront-eureka:latest .
-docker push garystafford/storefront-eureka:latest
+docker build -f Docker/Dockerfile --no-cache -t garystafford/storefront-eureka:3.0.0 .
+docker push garystafford/storefront-eureka:3.0.0
 
 # docker run --name storefront-eureka -d garystafford/storefront-eureka:latest
